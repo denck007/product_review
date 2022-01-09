aa4a150248da260284b20dc64d26978cbaab40a2
Neil Dencklau <denck007@umn.edu>
2022-01-08 18:46:55 -0600

    Update TagsBox to use bulma tags

--- frontend/src/components/TagsBox.vue
+++ frontend/src/components/TagsBox.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="columns is-multiline is-centered" align="center">
-    <div class="column" v-for="tag in tags" v-bind:key="tag.id">
-      <router-link :to="`/tags/${tag.slug}`" class="button is-primary">
-        {{ tag.name }}
-      </router-link>
-    </div>
+  <div class="tags is-centered">
+    <router-link
+      class="tag is-primary is-medium"
+      v-for="tag in tags"
+      v-bind:key="tag.id"
+      :to="`/tags/${tag.slug}`"
+    >
+      {{ tag.name }}
+    </router-link>
   </div>
 </template>
 
