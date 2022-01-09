20444ac58e4d1a23fdc5242db47ac20c3b8802b0
Neil Dencklau <denck007@umn.edu>
2022-01-09 14:19:10 -0600

    Does not work -Attempt at pagination

--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -13,6 +13,24 @@
       <div class="column is-9 box">
         <h2 class="subtitle">My reviews</h2>
         <Reviews v-bind:reviews="reviews" />
+        <b-pagination
+          :total="reviewsPagination.total"
+          v-model="currentPage"
+          :per-page="reviewsPagination.perPage"
+          order="is-centered"
+          range-before="3"
+          range-after="3"
+          icon-prev="chevron-left"
+          icon-next="chevron-right"
+          aria-next-label="Next page"
+          aria-previous-label="Previous page"
+          aria-page-label="Page"
+          aria-current-label="Current page"
+          iconPack="fa"
+          @change="getMyReviews"
+        >
+        </b-pagination>
+        <div>currentPage: {{ currentPage }}</div>
       </div>
     </div>
   </div>
@@ -25,17 +43,32 @@ import Reviews from "@/components/Reviews.vue";
 export default {
   name: "MyAccount",
   data() {
-    return { reviews: [] };
+    return {
+      reviews: [],
+      currentPage: 1,
+      reviewsPagination: {
+        total: 0,
+        perPage: 10,
+        currentPage: 1,
+      },
+    };
   },
   components: {
     Reviews,
   },
   mounted() {
     document.title = "Reviewer | My account";
-
     this.getMyReviews();
   },
+  watch: {
+    reviewsPagination: function (val) {
+      getMyReviews();
+    },
+  },
   methods: {
+    logClick() {
+      console.log("clicked");
+    },
     logout() {
       axios.defaults.headers.common["Authorization"] = "";
 
@@ -50,10 +83,14 @@ export default {
 
     async getMyReviews() {
       this.$store.commit("setIsLoading", true);
+      console.log("this.currentPage: " + this.currentPage);
       await axios
-        .get("/api/v1/reviews/")
+        .get(
+          `/api/v1/reviews/?limit=${this.reviewsPagination.perPage}&page=${this.currentPage}`
+        )
         .then((response) => {
-          this.reviews = response.data;
+          this.reviews = response.data.results;
+          this.reviewsPagination.total = response.data.count;
         })
         .catch((error) => {
           console.log(error);
